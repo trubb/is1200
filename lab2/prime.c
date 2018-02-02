f37b6484a955d3720f50624ec87987c44a34c8d2
erpette <erpette@kth.se>
2018-02-02 14:48:42 +0100

    task 1 and 2 finished

--- lab2/prime.c
+++ lab2/prime.c
@@ -12,50 +12,47 @@
 
 #include <stdio.h>
 
-/*
-	 function is_prime(n)
-     if n ≤ 1
-        return false
-     else if n ≤ 3
-        return true
-     else if n mod 2 = 0 or n mod 3 = 0
-        return false
-     let i ← 5
-     while i * i ≤ n
-        if n mod i = 0 or n mod (i + 2) = 0
-            return false
-        i ← i + 1
-     return true
-*/
-
 /**
- * is_prime changed to current state by erik pettersson 2018-01-31
- * determines if a number is prime by using the above defined methodology
+ * is_prime changed to current state by erik pettersson 2018-01-317
 **/
 int is_prime(int n){
- 
+
+
+	for (int i = 2; i <= (n/2); i++) { // anything below 2 is not prime
+		if (n % i == 0) {
+			return 0;
+		}
+	}
+
+	return 1;
+
+/* 
 	int i = 5;
 
 	if (n <= 1)	{	// 0 and 1 are not primes
 		return 0;
-	} else if (n <= 3) {	// 2 and 3 are primes
+	} else if (n >= 2) {	// 2 and 3 are primes
 		return 1;
 	} else if (n % 2 == 0 || n % 3 == 0) {	// if n mod 2 or n mod 3 is zero
 		return 0;	// then n is not prime
 	}
 
-	while (i * i <= n) {
-		if (n % i == 0 | n % (i + 2) == 0) {	// if n mod i or n mod i+2 is zero
+	while (i * i <= n) {	// as long as i squared below n then do:
+		if (n % i == 0 || n % (i + 2) == 0) {	// if n mod i or n mod i+2 is zero
 			return 0;	// then n is not prime
 		}
 		i++;	// increment loop variable
 	}
 
-	return 1;	// if it passes: should be prime
+	return 1;	// if it passes all above checks then:
+	*/
 }
 
 int main(void){
   printf("%d\n", is_prime(11));  // 11 is a prime.      Should print 1.
   printf("%d\n", is_prime(383)); // 383 is a prime.     Should print 1.
   printf("%d\n", is_prime(987)); // 987 is not a prime. Should print 0.
+  printf("%d\n", is_prime(5)); // 5 is a prime.			Should print 1.
+  printf("%d\n", is_prime(32)); // 32 is a prime.		Should print 0.
+  printf("%d\n", is_prime(32767)); //... is not a prime.Should print 0.
 }
--- lab2/print-primes.c
+++ lab2/print-primes.c
@@ -3,6 +3,10 @@
  By David Broman.
  Last modified: 2015-09-15
  This file is in the public domain.
+ 
+ Modified 2018-01-31
+ By Erik Pettersson
+
 */
 
 
@@ -10,36 +14,76 @@
 #include <stdlib.h>
 
 #define COLUMNS 6
+//////////////////////////////////////////
+//		Initialize global variables		//
+//////////////////////////////////////////
+int count = 0;	// initialize counter for number printing to 0
+
+void print_number(int n) {	// void function does things but does not explicitly return something
+
+	printf("%10d",n);	// print the number, 10 as it's what done below, space for characters?
+	
+	count++;			// every time we print, increase count by 1 to be able to..
+
+	if (count % COLUMNS == 0) {	// if value of counter / COLUMNS yields no remainder
+		printf("\n");			// force newline and continue printing
+	}
+}
 
 
-void print_primes(int n){
-  // Should print out all prime numbers less than 'n'
-  // with the following formatting. Note that
-  // the number of columns is stated in the define
-  // COLUMNS
+/**
+ * is_prime changed to current state by erik pettersson 2018-01-31
+**/
+int is_prime(int n) {
 
-  printf("%10d ", 2);
-  printf("%10d ", 3);
-  printf("%10d ", 5);
-  printf("%10d ", 7);
-  printf("%10d ", 11);
-  printf("%10d ", 13);
-  printf("\n");
-  printf("%10d ", 17);
-  printf("%10d ", 19);
+	int i = 0;
+	for (i = 2; i <= ( n/2 ); i++) {	// if we reach a value that is beyond 
+										//the range then n is prime, returns 1
+		if (n % i == 0) {	// if n / anything that isnt 2 or itself yields 0 remainder
+							// then n is not prime, return 0
+			return 0;
+		}
+	}
 
-  printf("\n");
+	return 1; // if it passes the above check it is prime, 1
+}
+
+void print_primes(int n) {
+	// Should print out all prime numbers less than 'n'
+	// with the following formatting. Note that
+	// the number of columns is stated in the define
+	// COLUMNS
+
+	for (int i = 2; i <= n; i++) {	// since 0, 1 are not primes, start at 2
+		if( is_prime(i) ) {
+			print_number(i);
+		}
+	}
+
+/*	printf("%10d ", 2);
+	printf("%10d ", 3);
+	printf("%10d ", 5);
+	printf("%10d ", 7);
+	printf("%10d ", 11);
+	printf("%10d ", 13);
+	printf("\n");
+	printf("%10d ", 17);
+	printf("%10d ", 19);
+*/
+	printf("\n");
 }
 
 // 'argc' contains the number of program arguments, and
 // 'argv' is an array of char pointers, where each
 // char pointer points to a null-terminated string.
-int main(int argc, char *argv[]){
-  if(argc == 2)
-    print_primes(atoi(argv[1]));
-  else
-    printf("Please state an interger number.\n");
-  return 0;
-}
+int main(int argc, char *argv[]) {
 
- 
+	if(argc == 2) {
+		print_primes(atoi(argv[1]));
+	}
+	else {
+		printf("Please state an interger number.\n");
+	}
+
+	return 0;
+}
